5abf44bd008e3bf9d8392b591ddd6eb8acc5be6d
nick shoup <shoupnb@gmail.com>
2023-08-30 19:53:24 -0600

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LnProxyApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LnProxyController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<LnProxyController> _logger;
+
+    public LnProxyController(ILogger<LnProxyController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetHodlInvoice")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
