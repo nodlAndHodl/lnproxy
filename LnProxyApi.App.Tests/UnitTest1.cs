5e3d4c7e051ca578fa7426af487b0a15e29843bb
nick shoup <shoupnb@gmail.com>
2023-11-05 15:35:40 -0700

    adding tests and seperate project

---
+++ LnProxyApi.App.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Logging;
+using LnProxyApi.Helpers;
+namespace LnProxyApi.App.Tests;
+
+public class LnProxyServicesTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    
+    }
+
+    [Test]
+    public void HexStringHelperTest()
+    {
+        var bytes = HexStringHelper.HexStringToByteString("f32039b06e834b65e6b1af17fd0217100176f14a3d0e4bed4becbe5058544415");
+        Assert.That(bytes.IsEmpty, Is.EqualTo(false));
+        Assert.That(bytes.Length, Is.EqualTo(32));
+        Assert.That(bytes.ToBase64().ToString(), Is.EqualTo("8yA5sG6DS2Xmsa8X/QIXEAF28Uo9DkvtS+y+UFhURBU="));
+    }
+}
